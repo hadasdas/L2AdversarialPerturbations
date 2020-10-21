8230983a9ac8d36d0b00e14e6da2c70acea80c2d
Hadas Sonnenfeld <hadas.schacham@mail.huji.ac.il>
2020-10-21 07:19:37 -0700

    Adversarial examples found using gradient descent on the input with small steps.

--- adversarialExamples1.py
+++ adversarialExamples1.py
@@ -3,18 +3,18 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
 
 
 class FullyConnectedNN:
     def __init__(self, num_of_layers, data, batch_size=128, epochs=5, num_of_input_nodes=784):
         self.num_of_layers = num_of_layers
         self.num_of_input_nodes = num_of_input_nodes
+        self.num_of_inner_nodes = 100
         self.batch_size = batch_size
         self.epochs = epochs
         self.data = data
         self.model = None
-        # TODO - THIS FIELD IS IN CASE WE WANT TO PLOT ANYTHING. IF NOT - ERASE!!!!!!
-        self.history = None
         self._set_model()
 
     def _set_model(self):
@@ -24,10 +24,10 @@ class FullyConnectedNN:
         """
         self.model = Sequential()
         # first inner layer gets the input:
-        self.model.add(Dense(200, input_dim=self.num_of_input_nodes, activation="relu",
+        self.model.add(Dense(self.num_of_inner_nodes, input_dim=self.num_of_input_nodes, activation="relu",
                              use_bias=False, kernel_initializer="glorot_normal"))
         for i in range(self.num_of_layers - 2):
-            self.model.add(Dense(200, activation="relu", use_bias=False,
+            self.model.add(Dense(self.num_of_inner_nodes, activation="relu", use_bias=False,
                                  kernel_initializer="glorot_normal"))
 
         self.model.add(Dense(1, use_bias=False, kernel_initializer="glorot_normal"))
@@ -36,8 +36,8 @@ class FullyConnectedNN:
         self.model.compile(optimizer='adam', loss='mse', metrics=["accuracy"])
 
     def fit_model(self):
-        self.history = self.model.fit(self.data.x_train, self.data.y_train, batch_size=self.batch_size,
-                                      epochs=self.epochs, validation_data=(self.data.x_test, self.data.y_test))
+        self.model.fit(self.data.x_train, self.data.y_train, batch_size=self.batch_size, epochs=self.epochs,
+                       validation_data=(self.data.x_test, self.data.y_test))
 
     def predict(self, image):
         return self.model.predict(image)
@@ -49,7 +49,6 @@ class FullyConnectedNN:
 
 class MnistData:
     num_of_features = 784
-    # num_of_digits = 10
 
     def __init__(self):
         (self.x_train, self.y_train), (self.x_test, self.y_test) = mnist.load_data()
@@ -103,12 +102,9 @@ class AdversarialGenerator:
         original_prediction = self.network.model(image)
         perturbed_image, prediction = self.create_perturbed_image_gd_step(image, etta)
         iteration = 0
-        while AdversarialGenerator.same_label(original_prediction, prediction) and iteration < 1000000:
+        while AdversarialGenerator.same_label(original_prediction, prediction):
             perturbed_image, prediction = self.create_perturbed_image_gd_step(perturbed_image, etta)
             iteration += 1
-        if iteration == 100000:
-            print("distance: ", np.linalg.norm(image - perturbed_image))
-            print("!!!!!!!!!!!!!!!!!!! DID NOT GO WELL!!!!!!!!!!!!!")
 
         return perturbed_image, prediction
 
@@ -125,22 +121,30 @@ class AdversarialGenerator:
 mnist_data = MnistData()
 num_of_iterations = 1000
 indices_array = np.random.choice(len(mnist_data.x_test), num_of_iterations)
-num_of_layers_array = [2, 4, 8, 16]
-# num_of_layers_array = [8]
-for layer_num in num_of_layers_array:
+for layer_num in range(2, 9):
     network1 = FullyConnectedNN(layer_num, mnist_data)
     network1.fit_model()
     network1.print_accuracy()
-    sum_of_distances = 0
+    distance_list = np.empty(num_of_iterations)
+    i = 0
     for index in indices_array:
         adversarial_gen = AdversarialGenerator(network1)
         perturbed_image1, prediction1 = adversarial_gen.iterate_gd_until_label_change(
             mnist_data.x_test[index].reshape(1, MnistData.num_of_features))
-        sum_of_distances += np.linalg.norm(mnist_data.x_test[index].reshape(1, MnistData.num_of_features) - perturbed_image1)
+        current_dist = np.linalg.norm(mnist_data.x_test[index].reshape(1, MnistData.num_of_features) - perturbed_image1)
+        distance_list[i] = current_dist
+        i += 1
 
-        print("distance of current iteration is: ",
-              np.linalg.norm(mnist_data.x_test[index].reshape(1, MnistData.num_of_features) - perturbed_image1))
+        print("distance of iteration ", i, " is: ", current_dist)
 
-    average = sum_of_distances / num_of_iterations
+    average = sum(distance_list) / num_of_iterations
     print("Average of distances between image and perturbation among " + str(num_of_iterations) +
           " testing images is: ", average)
+    print("Maximal distance was: ", max(distance_list))
+    n, bins, patches = plt.hist(x=distance_list, bins='auto', color='#0504aa', alpha=0.7, rwidth=0.85)
+    plt.grid(axis='y', alpha=0.75)
+    # str_x_label = 'Distance from example. Average: ' + str(average) + '. \nMaximal distance: '
+    # + str(max(distance_list))
+    plt.xlabel('Distance. num of layers ' + str(layer_num) + '. Average: ' + str(average))
+    plt.ylabel('Frequency')
+    plt.show()
